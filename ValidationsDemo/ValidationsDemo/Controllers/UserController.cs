5c7cc6ecfba7bc5282a4863d7473d5a1ec981ae3
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-21 18:38:02 +0530

    pushing new project

---
+++ ValidationsDemo/ValidationsDemo/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationsDemo.Models;
+namespace ValidationsDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                //  Your save logic goes here
+                return Content("You are register successfully");
+            }
+            else
+            {
+                return View("Register");
+            }
+        }
+    }
+}
---
+++ ValidationsDemo/ValidationsDemo/Models/Register.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ValidationsDemo.Models
+{
+    public class Register
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please Enter User Id")]
+        [Range(minimum:1,maximum:100,ErrorMessage ="Please Enter UserId between 1 to 100")]
+        [RegularExpression(pattern: @"^[1-9]\d*$",ErrorMessage ="Please Enter Digit")]
+        public int UserId { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please Enter User Name")]
+        [DataType(DataType.Text,ErrorMessage ="Name must be in Alphabets")]
+        [StringLength(maximumLength:50, MinimumLength =10)]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        //[RegularExpression(pattern: "/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" ,ErrorMessage ="Invalid Email")]
+        public string Email { get; set; }
+    }
+}
